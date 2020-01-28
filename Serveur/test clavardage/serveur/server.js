2db4891f3e8d596b3d48d2da946d618874f6c44b
maxime.bernier <maxime.bernier@polymtl.ca>
2020-01-28 16:40:16 -0500

    serveur: Mis en commentaire le sendFile

--- Serveur/test clavardage/serveur/server.js
+++ Serveur/test clavardage/serveur/server.js
@@ -1,9 +1,11 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+const path = require('path');
 
 app.get('/', function(req, res){
-  res.sendFile('/Users/AnisTelmat/Desktop/projet 3/client' + '/index.html');
+  // console.log(path.resolve(__dirname + '/../client/index.html'))
+  // res.sendFile(path.resolve(__dirname + '/../client/index.html'));
 });
 
 io.on('connection', function(socket){
