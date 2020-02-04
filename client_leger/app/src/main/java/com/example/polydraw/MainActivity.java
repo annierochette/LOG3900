672ccead1b14efb71f5cae566911048ae832b659
Annie <annie1.rochette@hotmail.com>
2020-02-04 15:06:33 -0500

    Added sendMessage and onMessage method header

--- client_leger/app/src/main/java/com/example/polydraw/MainActivity.java
+++ client_leger/app/src/main/java/com/example/polydraw/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.polydraw;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
+
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = (EditText) findViewById(R.id.EditText);
+    }
+
+    public void onMessage(com.example.polydraw.Message receivedMessage){
+
+    }
+
+    public void sendMessage(View view){
+        String message = editText.getText().toString();
+        if(message.length() > 0){
+
+        }
     }
 }
