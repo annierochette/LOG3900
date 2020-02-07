5ef89fb674c20db77fb62262a5b3fe44dcb5fe65
maxime.bernier <maxime.bernier@polymtl.ca>
2020-02-06 20:58:04 -0500

    server: Gérer les nom d'utilisateurs

---
+++ Serveur/prototype/server.js
@@ -0,0 +1,47 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+const path = require('path');
+
+var usernamesMap = new Map();
+var usernames = new Set();
+
+app.get('/', function(req, res){
+  // console.log(path.resolve(__dirname + '/../client/index.html'))
+  res.sendFile(path.resolve(__dirname + '/index.html'));
+});
+
+io.on('connection', function(socket){
+  usernamesMap.set(socket.id, "Anonymous");
+  console.log("User connected");
+
+  socket.on('chat message', function(msg){
+      var message = JSON.parse(msg);
+      message.timestamp = Date.now();
+      io.emit('chat message', JSON.stringify(message));
+  });
+  
+  io.on("changeUsername", function(username){
+    var nameUsed = usernames.has(username);  
+    if (nameUsed) {
+        socket.emit("changeUsername", {"accept":false});
+        socket.disconnect();
+    }
+    else {
+        usernamesMap.set(socket.id, username);
+        usernames.add(username);
+        socket.emit("changeUsername", {"accept":true});
+    }
+  });
+
+  io.on("disconnection", function() {
+      usernames.delete(usernamesMap.get(socket.id));
+      usernamesMap.delete(socket.id);
+      console.log("User disconnected.");
+  });
+
+});
+
+http.listen(8080, function(){
+  console.log('listening on *:8080');
+});
