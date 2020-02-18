f41ecff98266288073f4ef09f2573bec16ff9cc6
Florence <florencecimonpaquet@hotmail.com>
2020-02-18 11:00:38 -0500

    ajustement de l espace chatbox

--- Client_leger/app/src/main/java/com/example/polydraw/Menu.java
+++ Client_leger/app/src/main/java/com/example/polydraw/Menu.java
@@ -4,12 +4,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Menu extends AppCompatActivity {
     private Button playButton;
-    private Button profileButton;
+    private ImageButton profileButton;
     private Button tutorialButton;
     private Button settingsButton;
 
@@ -19,7 +20,7 @@ public class Menu extends AppCompatActivity {
         setContentView(R.layout.menu_layout);
 
         playButton = (Button) findViewById(R.id.play);
-        profileButton = (Button) findViewById(R.id.profile);
+        profileButton = (ImageButton) findViewById(R.id.profile);
         tutorialButton = (Button)findViewById(R.id.tutorial);
         settingsButton = (Button) findViewById(R.id.settings);
 
