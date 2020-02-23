944dcdad45af1b73e35a5e3fd7025817c368eed8
Florence <florencecimonpaquet@hotmail.com>
2020-02-22 21:29:40 -0500

    Ajout page signup

---
+++ Client_leger/app/src/main/java/com/example/polydraw/SignupActivity.java
@@ -0,0 +1,16 @@
+package com.example.polydraw;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+    }
+}
