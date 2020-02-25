1e330785d5ed629e818e5ecf57d77b7d77275167
Florence <florencecimonpaquet@hotmail.com>
2020-02-24 23:14:35 -0500

    ajout fonction envoyer formulaire seulement si parametres sont remplis et mot de passe est confirme

--- Client_leger/app/src/main/java/com/example/polydraw/SignupActivity.java
+++ Client_leger/app/src/main/java/com/example/polydraw/SignupActivity.java
@@ -2,15 +2,46 @@ package com.example.polydraw;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SignupActivity extends AppCompatActivity {
+
+    private Button signup;
+    private EditText name;
+    private EditText surname;
+    private EditText username;
+    private EditText password;
+    private EditText passwordConfirmation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        name = (EditText) findViewById(R.id.name);
+        surname = (EditText) findViewById(R.id.surname);
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        passwordConfirmation = (EditText) findViewById(R.id.passwordConfirmation);
+        signup = (Button) findViewById(R.id.signup);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                if(!name.getText().toString().trim().isEmpty() && !surname.getText().toString().trim().isEmpty() && !username.getText().toString().trim().isEmpty() && !password.getText().toString().trim().isEmpty() && !passwordConfirmation.getText().toString().trim().isEmpty()) {
+                    if(password.getText().toString().equals(passwordConfirmation.getText().toString())) {
+                        Intent intent = new Intent(SignupActivity.this, Menu.class);
+                        startActivity(intent);
+                    }
+                }
+            }
+        });
+
+
     }
+
 }
