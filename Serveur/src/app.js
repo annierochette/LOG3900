ba89936bffd447aaf8d23e3f87306e3126c92e03
maxime.bernier <maxime.bernier@polymtl.ca>
2020-03-04 12:53:54 -0500

    serveur: Ajout des routes pour ajouter, supprimer et obtenir un jeu

--- Serveur/src/app.js
+++ Serveur/src/app.js
@@ -4,9 +4,13 @@ require('./db/db')
 
 app.use(express.json({limit: "50mb"}));
 
-var usersRouter = require("./player/player.route");
-var generalStatsRouter = require("./player/general.stats.route");
+let usersRouter = require("./player/player.route");
+let generalStatsRouter = require("./player/general.stats.route");
+let gameRouter = require("./game/game.route");
+
+
 app.use(usersRouter);
 app.use(generalStatsRouter);
+app.use(gameRouter);
 
 module.exports = app;
