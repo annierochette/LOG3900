ebbadbe1354691244734b918f06289fe31114ab8
maxime.bernier <maxime.bernier@polymtl.ca>
2020-03-10 10:13:16 -0400

    TEstingp^kcfmsdklvmkldfvkle kerklg nedtnkl

--- Client_lourd/Paint/PolyPaint/Vues/DrawingWindow.xaml.cs
+++ Client_lourd/Paint/PolyPaint/Vues/DrawingWindow.xaml.cs
@@ -1,10 +1,16 @@
-﻿using System;
+﻿using PolyPaint.VueModeles;
+using Quobject.SocketIoClientDotNet.Client;
+using System;
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Controls.Primitives;
-using PolyPaint.VueModeles;
 using System.Windows.Controls;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.ObjectModel;
+using System.Windows.Ink;
 
 namespace PolyPaint.Vues
 {
@@ -13,10 +19,38 @@ namespace PolyPaint.Vues
     /// </summary>
     public partial class DrawingWindow : UserControl
     {
+        private readonly int BUCKET_SIZE = 25;
+        private List<Point> pointBucket = new List<Point>();
+        private Socket socket;
+
         public DrawingWindow()
         {
             InitializeComponent();
- 
+            socket = IO.Socket("http://127.0.0.1:5050");
+
+            socket.On("draw", points => {
+                Console.WriteLine("On draw: " + points);
+                StylusPointCollection scp = JsonConvert.DeserializeObject<StylusPointCollection>(points.ToString());
+                Stroke s = new Stroke(scp);
+                if (!Dispatcher.CheckAccess())
+                {
+                    Dispatcher.Invoke(() =>
+                    {
+                        s.DrawingAttributes = surfaceDessin.DefaultDrawingAttributes.Clone();
+                        surfaceDessin.Strokes.Add(s);
+                    });
+                }
+                else {
+                    s.DrawingAttributes = surfaceDessin.DefaultDrawingAttributes.Clone();
+                    surfaceDessin.Strokes.Add(s);
+                }
+            });
+
+            socket.On("modifyProperty", drawingAttribute => {
+                Console.WriteLine("MODIFY PROPERTY");
+            });
+
+
         }
         
         // Pour gérer les points de contrôles.
@@ -35,6 +69,53 @@ namespace PolyPaint.Vues
         {
             Point p = e.GetPosition(surfaceDessin);
             textBlockPosition.Text = Math.Round(p.X) + ", " + Math.Round(p.Y) + "px";
+
+            if (surfaceDessin.IsMouseCaptured)
+            {
+                pointBucket.Add(p);
+
+                if (pointBucket.Count.Equals(BUCKET_SIZE))
+                {
+                    uploadPoints();
+                }
+            } else if (pointBucket.Count > 0) 
+            {
+                uploadPoints();
+            }
+
+        }
+
+        private void uploadPoints()
+        {
+            string points = JsonConvert.SerializeObject(pointBucket, new PointConverter());
+            socket.Emit("draw", "General", points);
+            pointBucket.Clear();
+        }
+
+        private class PointConverter : JsonConverter
+        {
+            public override void WriteJson(
+                JsonWriter writer, object value, JsonSerializer serializer)
+            {
+                var point = (Point)value;
+
+                serializer.Serialize(
+                    writer, new JObject { { "X", point.X }, { "Y", point.Y } });
+            }
+
+            public override object ReadJson(
+                JsonReader reader, Type objectType, object existingValue,
+                JsonSerializer serializer)
+            {
+                var jObject = serializer.Deserialize<JObject>(reader);
+
+                return new StylusPoint((double)jObject["X"], (double)jObject["Y"]);
+            }
+
+            public override bool CanConvert(Type objectType)
+            {
+                return objectType == typeof(Point);
+            }
         }
 
     }
