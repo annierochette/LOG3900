1c601983eabee35dc107f97d33ce1af9ebd6e78d
maxime.bernier <maxime.bernier@polymtl.ca>
2020-03-13 09:57:03 -0400

    WIP

---
+++ Client_lourd/Paint/PolyPaint/Modeles/CustomDynamicRenderer.cs
@@ -0,0 +1,51 @@
+﻿using PolyPaint.Utilitaires;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Input.StylusPlugIns;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace PolyPaint.Modeles
+{
+    public class CustomDynamicRenderer: System.Windows.Input.StylusPlugIns.DynamicRenderer
+    {
+
+        private static AppSocket socket = AppSocket.Instance;
+        private string path;
+        
+
+        public CustomDynamicRenderer(): base()
+        {
+
+        }
+
+        protected override void OnStylusDown(RawStylusInput rawStylusInput)
+        {
+            base.OnStylusDown(rawStylusInput);
+        }
+
+        protected override void OnStylusUp(RawStylusInput rawStylusInput)
+        {
+            base.OnStylusUp(rawStylusInput);
+        }
+
+        protected override void OnDraw(DrawingContext drawingContext,
+                                   StylusPointCollection stylusPoints,
+                                   Geometry geometry, Brush fillBrush)
+        {
+            socket.Emit("draw", "General", geometry.ToString());            
+            base.OnDraw(drawingContext, stylusPoints, geometry,fillBrush);
+        }
+
+        private void appendGeometry(string toAppend)
+        {
+            path = path.Remove(path.Length - 1);
+            path += toAppend.Remove(0, 3);
+        }
+    }
+}
---
+++ Client_lourd/Paint/PolyPaint/Modeles/CustomInkCanvas.cs
@@ -0,0 +1,60 @@
+﻿using PolyPaint.Utilitaires;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace PolyPaint.Modeles
+{
+    public class CustomInkCanvas : System.Windows.Controls.InkCanvas
+    {
+        private static AppSocket socket = AppSocket.Instance;
+
+        public CustomInkCanvas() : base()
+        {
+            this.DynamicRenderer = new CustomDynamicRenderer();
+            //int index = this.StylusPlugIns.IndexOf(this.DynamicRenderer);
+            //Console.WriteLine("Index: " + index + ", #plugIns: " + this.StylusPlugIns.Count);
+            //this.StylusPlugIns.Insert(index, new DraftsmanPlugIn());
+            //this.StylusPlugIns.RemoveAt(0);
+            //Console.WriteLine("Index: " + index + ", #plugIns: " + this.StylusPlugIns.Count);
+            socket.On("draw", (geometry) =>
+            {
+                Dispatcher.Invoke(() =>
+                {
+                    Path path = new Path();
+                    path.Data = Geometry.Parse((string)geometry);
+                    Console.WriteLine("Path data: " + path.Data);
+                    //Children.Add(path);
+                    PointAnimationUsingPath p = new PointAnimationUsingPath();
+                    p.PathGeometry = (PathGeometry) path.Data;
+                    //   Console.WriteLine("Geometry received: " + geometry);
+                    //   Console.WriteLine("Children: " + Children.Count);            
+                });
+            });
+        }
+
+        private StylusPointCollection extractStylusPointsFromPath(string data)
+        {
+            StylusPointCollection spc = new StylusPointCollection();
+            data = data.Remove(0, 3);
+            data = data.Remove(data.Length - 1);
+            data = data.Replace("C", ",").Replace(" ", ",");
+            string[] splittedData = data.Split(',');
+            Console.WriteLine("Data: " + data);
+            for (int i = 0; i < splittedData.Length; i += 2)
+            {
+                spc.Add(new StylusPoint(Convert.ToDouble(splittedData[i]) + 10, Convert.ToDouble(splittedData[i + 1]) + 10));
+            }
+
+            return spc;
+        }
+
+    }
+}
