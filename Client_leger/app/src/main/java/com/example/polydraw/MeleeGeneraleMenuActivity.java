e50ddedf6a348e0d64d255ef4a63b5526d88132a
Florence <florencecimonpaquet@hotmail.com>
2020-03-24 12:39:39 -0400

    ajout menu melee generale

--- Client_leger/app/src/main/java/com/example/polydraw/MeleeGeneraleMenuActivity.java
+++ Client_leger/app/src/main/java/com/example/polydraw/MeleeGeneraleMenuActivity.java
@@ -1,7 +1,6 @@
 package com.example.polydraw;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentActivity;
 
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
@@ -11,25 +10,21 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
-public class PlayMenu extends AppCompatActivity {
+public class MeleeGeneraleMenuActivity extends AppCompatActivity {
 
     private Button backButton;
-    private Button multiplayerButton;
-    private Button soloButton;
-    private Button freeButton;
+    private Button playButton;
     private ImageButton disconnectButton;
     private ImageView chatButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_play_menu);
+        setContentView(R.layout.activity_melee_generale_menu);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE);
 
         backButton = (Button) findViewById(R.id.backButton);
-        multiplayerButton = (Button) findViewById(R.id.multiplayerButton);
-        soloButton = (Button) findViewById(R.id.soloButton);
-        freeButton = (Button) findViewById(R.id.freeButton);
+        playButton = (Button) findViewById(R.id.playButton);
         disconnectButton = (ImageButton) findViewById(R.id.logoutButton);
         chatButton = (ImageView) findViewById(R.id.chatButton);
 
@@ -40,24 +35,10 @@ public class PlayMenu extends AppCompatActivity {
             }
         });
 
-        multiplayerButton.setOnClickListener(new View.OnClickListener() {
+        playButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                goToMultiplayerGameMenu();
-            }
-        });
-
-        soloButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                goToSoloGameMenu();
-            }
-        });
-
-        freeButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openFreeGame();
+                playMultiplayerGame();
             }
         });
 
@@ -75,7 +56,7 @@ public class PlayMenu extends AppCompatActivity {
             }
         });
 
-        }
+    }
 
 
 
@@ -84,18 +65,8 @@ public class PlayMenu extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void goToMultiplayerGameMenu(){
-        Intent intent = new Intent(this, MeleeGeneraleMenuActivity.class);
-        startActivity(intent);
-    }
-
-    public void goToSoloGameMenu(){
-        Intent intent = new Intent(this, ModeSoloActivity.class);
-        startActivity(intent);
-    }
-
-    public void openFreeGame(){
-        Intent intent = new Intent(this, DrawingActivity.class);
+    public void playMultiplayerGame(){
+        Intent intent = new Intent(this, meleegeneraleActivity.class);
         startActivity(intent);
     }
 
