b61bd7c81bd2d45626faf683e1bca501fc51e0a7
matela <manyl.telmat@polymtl.ca>
2020-04-10 14:33:03 -0400

    merge finally fixed

--- Serveur/src/app.js
+++ Serveur/src/app.js
@@ -7,10 +7,14 @@ app.use(express.json({limit: "50mb"}));
 let usersRouter = require("./player/player.route");
 let generalStatsRouter = require("./player/general.stats.route");
 let gameRouter = require("./game/game.route");
+let matchRouter = require("./match/match.route");
+
 
 
 app.use(usersRouter);
 app.use(generalStatsRouter);
 app.use(gameRouter);
+app.use(matchRouter);
+
 
 module.exports = app;
