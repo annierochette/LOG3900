98b7a2cad0a479e0ec769a9e9b5cd798f4140f63
matela <manyl.telmat@polymtl.ca>
2020-04-10 14:40:23 -0400

    ajout de quickdraw

--- Serveur/src/app.js
+++ Serveur/src/app.js
@@ -8,13 +8,15 @@ let usersRouter = require("./player/player.route");
 let generalStatsRouter = require("./player/general.stats.route");
 let gameRouter = require("./game/game.route");
 let matchRouter = require("./match/match.route");
-
+let quickdrawRouter = require("./game/quickdraw.route");
 
 
 app.use(usersRouter);
 app.use(generalStatsRouter);
 app.use(gameRouter);
 app.use(matchRouter);
+app.use(quickdrawRouter);
+
 
 
 module.exports = app;
