4edc4b6ef5438bd098f343d97438ebbcc69ce922
maxime.bernier <maxime.bernier@polymtl.ca>
2020-04-13 23:12:59 -0400

    changer timestamp en edt

--- Serveur/src/utils/timestamp.js
+++ Serveur/src/utils/timestamp.js
@@ -1,9 +1,13 @@
+const moment = require("moment-timezone");
+
 exports.currentDate = function() {
     return new Date();
 }
 
 exports.chatString = function(timestamp) {
-    return (" à " + timestamp.getHours()
-        + ":" + timestamp.getMinutes()
-        + ":" + timestamp.getSeconds()); 
+    var myTimezone = "America/New_York";
+    var myDatetimeFormat= "YYYY-MM-DD hh:mm:ss a z";
+    var myDatetimeString = moment(timestamp).tz(myTimezone).format(myDatetimeFormat);
+    return (" à " + myDatetimeString); 
 }
+
